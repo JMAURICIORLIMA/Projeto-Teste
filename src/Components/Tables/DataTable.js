4b533d7ac5c1fe6ef3f7d6eae86bd4439973f1e3
JMAURICIORLIMA <mauricio.rodriguesdelima@gmail.com>
2021-10-11 11:39:22 -0300

    Finalização do projeto

---
+++ src/Components/Tables/DataTable.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import { Table, Button } from 'reactstrap';
+import ModalForm from '../Modals/Modal'
+
+class DataTable extends Component {
+
+  deleteItem = id => {
+    let confirmDelete = window.confirm('Deseja Deletar?')
+    if(confirmDelete){
+      fetch('https://mv-teste.herokuapp.com/employee', {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        id
+      })
+    })
+      .then(response => response.json())
+      .then(item => {
+        this.props.deleteItemFromState(id)
+      })
+      .catch(err => console.log(err))
+    }
+
+  }
+
+  render() {
+
+    const items = this.props.items.map(item => {
+      return (
+        <tr key={item.id}>
+          <th scope="row">{item.id}</th>
+          <td>{item.cafe}</td>
+          <td>{item.nome}</td>
+          <td>{item.cpf}</td>
+          <td>
+            <div style={{width:"110px"}}>
+              <ModalForm buttonLabel="Edit" item={item} updateState={this.props.updateState}/>
+              {' '}
+              <Button color="danger" onClick={() => this.deleteItem(item.id)}>Del</Button>
+            </div>
+          </td>
+        </tr>
+        )
+      })
+
+    return (
+      <Table responsive hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>CAFÉ</th>
+            <th>NOME</th>
+            <th>CPF</th>
+            <th>AÇÕES</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items}
+        </tbody>
+      </Table>
+    )
+  }
+}
+
+export default DataTable
